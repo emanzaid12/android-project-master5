70c76979d0782137096e99a283865edff7fa83e2
emanzaid12 <emanzaid139@gmail.com>
2024-04-27 00:46:16 +0300

    my Routes

--- newtask/lib/main.dart
+++ newtask/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:newtask/features/auth/login/view/loginPage.dart';
 import 'package:newtask/features/auth/onboarding/view/page/onboarding_page.dart';
+import 'package:newtask/features/auth/signup/view/signUpPage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -22,7 +23,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: onBoarding ? const loginPage() : const OnBoardingPage(),
+      onGenerateRoute: MyRoutes.onGenerateRoute,
+      onGenerateInitialRoutes: (_) => MyRoutes.initRoutes,
     );
   }
 }
+class MyRoutes {
+  static List<Route> initRoutes = [
+    MaterialPageRoute<dynamic>(
+      builder: (BuildContext context) => const OnBoardingPage(),
+    ),
+    MaterialPageRoute<dynamic>(
+      builder: (BuildContext context) => const signUpPage(),
+    ),
+  ];
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    //settings.arguments
+    switch (settings.name) {
+      case 'login':
+        
+        return MaterialPageRoute<dynamic>(
+          builder: (BuildContext context) => const loginPage(),
+        );
+      case 'signup':
+        return MaterialPageRoute<dynamic>(
+          builder: (BuildContext context) => const signUpPage(),
+        );
+      default:
+        return MaterialPageRoute<dynamic>(
+          builder: (BuildContext context) => const OnBoardingPage(),
+        );
+    }
+  }
+}
+
