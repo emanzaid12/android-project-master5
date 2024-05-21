11569e93f9bf396ed1dc66695a0d29c73352da5c
emanzaid12 <emanzaid139@gmail.com>
2024-05-21 19:03:50 +0300

    SharedPreferences

--- newtask/lib/core2/cubit/parentt_cubit.dart
+++ newtask/lib/core2/cubit/parentt_cubit.dart
@@ -1,41 +1,56 @@
 import 'dart:convert';
 
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 part 'parentt_state.dart';
 
 class ParenttCubit extends Cubit<ParenttState> {
-  static final ParenttCubit instance=ParenttCubit();
-  ThemeMode themeMode=ThemeMode.dark;
-  ParenttCubit() : super(ParenttInitial()){
-    loadLanguage();
+  static final ParenttCubit instance = ParenttCubit();
+  ThemeMode themeMode = ThemeMode.dark;
+  String lang = 'ar';
+  Map<String, dynamic> local = {};
+
+  ParenttCubit() : super(ParenttInitial()) {
+    _loadPreferences();
   }
-  Map<String,dynamic> local ={};
-  String lang='ar';
+
+  Future<void> _loadPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    themeMode = (prefs.getBool('isDarkMode') ?? true)
+        ? ThemeMode.dark
+        : ThemeMode.light;
+    lang = prefs.getString('lang') ?? 'ar';
+    await loadLanguage();
+    emit(ParenttInitial());
+  }
+
+  Future<void> _savePreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', themeMode == ThemeMode.dark);
+    prefs.setString('lang', lang);
+  }
+
   Future<void> loadLanguage() async {
-    String s=await rootBundle.loadString('assets/lang/$lang.json');
-    local=json.decode(s);
+    String s = await rootBundle.loadString('assets/lang/$lang.json');
+    local = json.decode(s);
     emit(ParenttInitial());
   }
-  void  changMode(){
-    if(themeMode==ThemeMode.dark){
-      themeMode=ThemeMode.light;
-    }
-    else{
-      themeMode=ThemeMode.dark;
-    }
+
+  void changMode() {
+    themeMode = themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    _savePreferences();
     emit(ParenttInitial());
   }
+
   void changLang() {
-    if (lang == 'ar') {
-     lang='en';
-    } else {
-     lang = 'ar';
-    }
-     loadLanguage();
+    lang = lang == 'ar' ? 'en' : 'ar';
+    loadLanguage();
+    _savePreferences();
     emit(ParenttInitial());
   }
 }
